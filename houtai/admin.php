72093d5159d11db6591a4222798cb31fd5c25df9
wangjin@wangjin.me <wangjin@wangjin.me@ef5e9175-8857-26e5-54fb-f5b857585499>
2011-03-23 08:04:52 +0000

    houtai added

---
+++ houtai/admin.php
@@ -0,0 +1,10 @@
+<?php
+$title = "Administration Panel";
+include("{$_SERVER['DOCUMENT_ROOT']}/header.php")
+?>
+
+<p>Administration Panel</p>
+
+<?php
+include("{$_SERVER['DOCUMENT_ROOT']}/footer.php")
+?>
